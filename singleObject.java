faf959d289932c692e34a266a66de69dddba4d46
FilipeAmorim <72051220+FilipeAmorim@users.noreply.github.com>
2020-09-28 20:54:00 -0300

    Singleton

---
+++ singleObject.java
@@ -0,0 +1,14 @@
+public final class singleObject {
+
+
+private static final singleObject instance = new singleObject();
+
+private singleObject(){}
+
+public static singleObject getInstance(){
+   return instance;
+}
+
+public void showMessage(){
+   System.out.println("Hello World!");
+}
